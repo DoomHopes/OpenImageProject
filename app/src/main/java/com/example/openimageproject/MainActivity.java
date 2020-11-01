0b2fd483f092adc663cc5597842967530c9d4ac4
Doom_hopes <diadia1911@ukr.net>
2020-11-01 11:02:23 +0200

    image path

--- app/src/main/java/com/example/openimageproject/MainActivity.java
+++ app/src/main/java/com/example/openimageproject/MainActivity.java
@@ -2,33 +2,50 @@ package com.example.openimageproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int OPEN_IMAGE_REQUEST_CODE = 101;
+    public TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
     }
 
     public void onClickOpenImg(View view) {
         Intent openImageDialog = new Intent(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(openImageDialog, OPEN_IMAGE_REQUEST_CODE);
+        openImageDialog.setType("image/*");
+        try {
+            startActivityForResult(openImageDialog, OPEN_IMAGE_REQUEST_CODE);
+        }
+        catch (ActivityNotFoundException ex){
+            textView.setText(ex.getMessage());
+        }
+
     }
 
-    protected void OnActivityResult(int requestCode, int resultCode, Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
 
         if(data == null) return;
 
         switch (requestCode){
             case OPEN_IMAGE_REQUEST_CODE:
-
+                if(resultCode == RESULT_OK){ //Активность закрыта кнопкой ОК
+                    String fileName = data.getData().getEncodedPath();
+                    textView.setText(fileName);
+                }
                 break;
         }
     }
